8d8448d417a6fd975b8748da622e2b42d212c2d1
Kagumi <adrienjuttaud@gmail.com>
2025-10-20 15:10:02 +0200

    fin input et gestion vaisseau

--- Assets/Scripts/SpaceShipController.cs
+++ Assets/Scripts/SpaceShipController.cs
@@ -1,12 +1,20 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem.LowLevel;
 
 public class SpaceShipController : MonoBehaviour, ControllerAll
 {
-    public float speed;
+    public float speedFactor;
+    public InputManager inputManager;
+    public float speedRotation;
+    public float speedMove;
     public GameObject rightMotor;
     public GameObject leftMotor;
 
+    private void Start()
+    {
+        inputManager.setController(this);
+    }
     public void PlayerOnePress()
     {
         leftMotor.SetActive(true);
@@ -17,12 +25,30 @@ public class SpaceShipController : MonoBehaviour, ControllerAll
     }
     public void PlayerTwoPress()
     {
-
         rightMotor.SetActive(true);
     }
     public void PlayerTwoRelease()
     {
-
         rightMotor.SetActive(false);
+    }
+
+    private void Update()
+    {
+        float rotationSens = 0;
+        if (rightMotor.activeSelf)
+        {
+            rotationSens += 1;
+        }
+        if (leftMotor.activeSelf)
+        {
+            rotationSens -= 1;
+        }
+        transform.Rotate(0, 0, rotationSens * Time.deltaTime * speedRotation);
+
+        float rotation = transform.localEulerAngles.z - 180;
+        float direction = Mathf.Sign(rotation);
+        float puissance =  1 - Mathf.Abs(Mathf.Abs(rotation) - 90) / 90;
+        Vector3 translation = new Vector3(direction * puissance, 0, 0);
+        transform.position += translation * Time.deltaTime * speedMove;
     }
 }
